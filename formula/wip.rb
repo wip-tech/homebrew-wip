f5ba34785c78975dcc3710f6c212fa1ad268ffd4
Guy Tavor <guy@wip.tech>
2022-07-17 17:43:02 +0300

    initial brew experiment
    GT-2

--- formula/wip.rb
+++ formula/wip.rb
@@ -1,4 +1,5 @@
 require "formula"
+require_relative "lib/private_strategy"
 
 class Wip < Formula
   desc "Wip"
@@ -6,13 +7,14 @@ class Wip < Formula
   url "https://github.com/wip-tech/wip/releases/download/v0.0.29/wip-v0.0.29-x86_64-apple-darwin.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "10fc75cd82f4c452c8402f2bc5d063c875a8560cfcbbab789164abfa83bae4c1"
   head "https://github.com/wip-tech/wip.git"
+  version "0.0.29"
 
   def install
     bin.install "wip"
   end
 
-  # Homebrew requires tests.
-  test do
-    assert_match "ok: wip v0.0.29 [GT-266-default#cd1f84a2-modified] (built: 2022-07-17T13:38:13.897859+00:00)", shell_output("#{bin}/wip check-version", 0)
-  end
+  # # Homebrew requires tests.
+  # test do
+  #   assert_match "ok: wip v0.0.29 [GT-266-default#cd1f84a2-modified] (built: 2022-07-17T13:38:13.897859+00:00)", shell_output("#{bin}/wip check-version", 0)
+  # end
 end
