1b83a35e2e7840ee0cbb66fff04ea34d10cff690
Guy Tavor <guy@wip.tech>
2022-07-17 17:28:13 +0300

    initial brew experiment
    GT-0

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# homebrew-wip
+# Homebrew distribution for our private repository
+
+I Followed this [blog post](https://medium.com/prodopsio/creating-homebrew-taps-for-private-internal-tools-c41363d58ab0)
---
+++ formula/wip.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Wip < Formula
+  desc "Wip"
+  homepage "https://wip.tech"
+  url "https://github.com/wip-tech/wip/releases/download/v0.0.29/wip-v0.0.29-x86_64-apple-darwin.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "10fc75cd82f4c452c8402f2bc5d063c875a8560cfcbbab789164abfa83bae4c1"
+  head "https://github.com/wip-tech/wip.git"
+
+  def install
+    bin.install "wip"
+  end
+
+  # Homebrew requires tests.
+  test do
+    shell_output("#{bin}/wip --check-install", 0).starts_with("ok: wip v0.0.29")
+  end
+end
