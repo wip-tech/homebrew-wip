1615ec3b447be76d858be3b01a42bf7a05e62cff
Guy Tavor <guy@wip.tech>
2022-07-27 07:23:05 +0300

    v0.0.35
    GT-24

--- Casks/wip.rb
+++ Casks/wip.rb
@@ -2,9 +2,9 @@
 cask "wip" do
   desc "Wip makes software development simpler, more enjoyable, and more productive"
   homepage "https://wip.tech"
-  url "https://github.com/wip-tech/wip-dist/releases/download/v0.0.34/wip-v0.0.34-x86_64-apple-darwin.tar.gz"
-  sha256 "3d6208efbb4b664ccf8e25bb96dbf0c829dcadd7c72caf0cb7e5253982f51be2"
-  version "0.0.34"
+  url "https://github.com/wip-tech/wip-dist/releases/download/v0.0.35/wip-v0.0.35-x86_64-apple-darwin.tar.gz"
+  sha256 "c645ec165e10d1bf1493364f635088544ec710e04ab58ced386c83c54302fc28"
+  version "0.0.35"
 
   app "wip.app"
   binary "wip"
