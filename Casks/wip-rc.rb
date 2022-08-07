7018813a4f0b87339b3d35c81a95627b56035906
Guy Tavor <guy@scoompa.com>
2022-08-07 17:02:13 +0300

    RC: v0.0.39
    GT-31

--- Casks/wip-rc.rb
+++ Casks/wip-rc.rb
@@ -1,5 +1,5 @@
 # see: https://docs.brew.sh/Cask-Cookbook
-cask "wip" do
+cask "wip-rc" do
   desc "Wip makes software development simpler, more enjoyable, and more productive"
   homepage "https://wip.tech"
   url "https://github.com/wip-tech/wip-dist/releases/download/v0.0.39/wip-v0.0.39-x86_64-apple-darwin.tar.gz"
