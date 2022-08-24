fcd84c63f32965130196340d743702a3d89caf9a
Guy Tavor <guy@scoompa.com>
2022-08-24 12:08:49 +0300

    RC: v0.0.48
    GT-41

--- Casks/wip-rc.rb
+++ Casks/wip-rc.rb
@@ -2,9 +2,9 @@
 cask "wip-rc" do
   desc "Wip makes software development simpler, more enjoyable, and more productive"
   homepage "https://wip.tech"
-  url "https://github.com/wip-tech/wip-dist/releases/download/v0.0.46/wip-v0.0.46-x86_64-apple-darwin.tar.gz"
-  sha256 "8c4976e99738c3e0fbb844c5a7e7b8edabaa91897baee7d272fb13703e8f2de1"
-  version "0.0.46"
+  url "https://github.com/wip-tech/wip-dist/releases/download/v0.0.48/wip-v0.0.48-x86_64-apple-darwin.tar.gz"
+  sha256 "cd84af12dc9d2fa460944afe09f182005b4b6559c4db75cdfa4ba45f025b326f"
+  version "0.0.48"
 
   app "wip.app"
   binary "wip"
