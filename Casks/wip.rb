a69bb0111891b270d4529f1c086f96753123d5f7
Guy Tavor <guy@scoompa.com>
2022-09-14 16:08:24 +0300

    Release: v0.1.2
    GT-49

--- Casks/wip.rb
+++ Casks/wip.rb
@@ -2,11 +2,11 @@
 cask "wip" do
   desc "Wip makes software development simpler, more enjoyable, and more productive"
   homepage "https://wip.tech"
-  url "https://github.com/wip-tech/wip-dist/releases/download/v0.0.51/wip-v0.0.51-x86_64-apple-darwin.tar.gz"
-  sha256 "f3442b7f5724de345a1137d3688b3c5f0cc62d1453a9d22b8e671b02b091e4b1"
-  version "0.0.51"
+  url "https://github.com/wip-tech/wip-dist/releases/download/v0.1.2/highflux-v0.1.2-x86_64-apple-darwin.tar.gz"
+  sha256 "9bbf3009b92d118caca81f9395637eb70d79ede83738a87626205efde3b5249d"
+  version "0.1.2"
 
-  app "wip.app"
-  binary "wip"
+  app "HighFlux.app"
+  binary "hf"
 
 end
