951d8f310ff4488447c24b28a46711e424e6a40c
Mathijs Vogelzang <mathijs@wip.tech>
2022-12-06 15:33:31 +0100

    Release: 0.4.0

--- Casks/highflux.rb
+++ Casks/highflux.rb
@@ -1,9 +1,9 @@
 # see: https://docs.brew.sh/Cask-Cookbook
 cask "highflux" do
-  version "0.3.0"
-  sha256 "b98ef4f44ca38e42471d84ee3c4cb23e929a119c2a3dba580eb9b114482e6c6d"
+  version "0.4.0"
+  sha256 "f99d6e0b94a8149ee2edbd3f5efbeee21b8669fa77d9847fb5960bb932ab0175"
 
-  url "https://www.highflux.io/download/highflux_0.3.0_apple_x64.dmg", referer: 'Homebrew'
+  url "https://www.highflux.io/download/highflux_0.4.0_apple_x64.dmg", referer: 'Homebrew'
   name "HighFlux"
   desc "HighFlux makes software development simpler, more enjoyable, and more productive"
   homepage "https://www.highflux.io"
